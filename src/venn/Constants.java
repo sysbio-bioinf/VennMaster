72aa6ccc3240794d360df0bdf776ce223009b3f2
Sebastian Behrens <sebastian.behrens@uni-ulm.de>
2013-08-30 10:40:01 +0200

    changed version constant to 0.38.0

--- src/venn/Constants.java
+++ src/venn/Constants.java
@@ -4,8 +4,8 @@ public class Constants {
     public static final int MAX_NUM_GROUPS = 10;       	// initial maximum number of groups (before warning)
     public static final int WHICH_NELEMENTS_LOG = 2;		// which logarithm to use
     public static final int VERSION_MAJOR = 0;          // version number
-    public static final int VERSION_MINOR = 37;
-    public static final int VERSION_SUB   = 5;
-    public static final String VERSION_DATE = "2010-11-17";
+    public static final int VERSION_MINOR = 38;
+    public static final int VERSION_SUB   = 0;
+    public static final String VERSION_DATE = "2013-08-29";
     
 }
